3b7abeaf130b8608ae26b72ef38ceb6a3bacb0ae
SwamiSankar <swamisankar2008@gmail.com>
2022-04-25 18:34:24 +0530

    Modification to module

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
